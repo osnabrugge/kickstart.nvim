30966ee9b9dfda322a705b96760043f6dfbb2b6b
Sean van Osnabrugge <18647660+osnabrugge@users.noreply.github.com>
2025-06-19 19:16:52 -0400

    feat: load kickstart plugins after setting leader keys

--- init.lua
+++ init.lua
@@ -93,6 +93,8 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+require("kickstart.plugins")
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
