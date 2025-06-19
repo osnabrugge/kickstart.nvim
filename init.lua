11d82d282109be0a6350cfc72b1d799036a6cf4e
Sean van Osnabrugge <18647660+osnabrugge@users.noreply.github.com>
2025-06-19 18:06:11 -0400

    fix: enable clipboard support and set Nerd Font to true in init.lua

--- init.lua
+++ init.lua
@@ -89,9 +89,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.opt.clipboard = "unnamedplus"
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
